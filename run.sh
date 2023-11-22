641bc3449eff2b25411e55e17bb319ed7c20c0d9
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2023-11-21 20:33:40 -0600

    [run.sh] added debug mode
    caller can run it in debug mode and have stacktraces and the like, or
    not pass the script anything and app will just run normally

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
-set -xe
+[ "$1" = "debug" ] && set -x
+set -e
 
-APP_PORT=:9000 ./go-todo
+PORT_TO_RUN_ON=:9000
+
+if [ "$1" = "debug" ]; then
+    GOTRACEBACK=crash APP_PORT=$PORT_TO_RUN_ON ./go-todo
+else
+    APP_PORT=$PORT_TO_RUN_ON ./go-todo
+fi
