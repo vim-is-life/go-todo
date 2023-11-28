9ec20caa2c495a861e9702220b5dd9dbc87fe676
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2023-11-27 19:33:54 -0600

    [api] added docs for how api works

--- controller/api.go
+++ controller/api.go
@@ -1,3 +1,23 @@
+// api.go is the API implementation for interactions with the backend outside of the browser.
+// The format for communication will be semicolon-delimited (;) CSV. Since we
+// have tabular data, CSV works well for us. Also, compared to JSON, CSV is
+// smaller and seems to be faster to serialize and deserialize.
+//
+// Specs for the CSV format used to communicate:
+// - it will have no header
+// - it will have 5 columns
+//   - column 1 will be unsigned int
+//   - column 2 will be string
+//   - column 3 will be string
+//   - column 4 will be int matching the underlying values of the TodoKind type
+//     (see model/defs)
+//   - column 5 will be int matching the underlying values of the TodoState type
+//     (see model/defs)
+//
+// An example response to get all the todos from the api, for instance, could
+// look like the following:
+// 3;finish todo app;;-1;-1
+// 1;learn rust;learn rust to ascend to a higher plane;0;0
 package controller
 
 import (
